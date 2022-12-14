e3ba2433605cc63e1ee362083998558c57be5904
Soheil Salimi <soheilsalimidev@gmail.com>
2022-12-14 22:25:59 +0330

    feat:design main page
    extract zip files

--- src/main/java/com/file/filemanager/HelloController.java
+++ src/main/java/com/file/filemanager/HelloController.java
@@ -1,14 +1,38 @@
 package com.file.filemanager;
 
+import com.file.filemanager.Utils.Util;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeTableView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HelloController {
+    String rootPath = "";
     @FXML
-    private Label welcomeText;
+    private MenuItem newFile;
+    @FXML
+    private MenuItem sortByYear;
+    @FXML
+    private MenuItem sortByFormat;
+    @FXML
+    private TreeTableView treeView;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onNewFileClick() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("zip files (*.zip)", "*.zip");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(newFile.getParentPopup().getOwnerWindow());
+
+        if (file != null)
+            try {
+                rootPath = Util.extractFolder(file.getPath(), file.getParent() + "//root//");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
